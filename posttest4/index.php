bf76ef4ae7d3b5ff94d78950c2ff839ae33ec2b3
muhammadnandaaf <113959841+muhammadnandaaf@users.noreply.github.com>
2022-10-11 23:22:48 +0800

    Add files via upload

---
+++ posttest4/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register</title>
+</head>
+<body>
+    <div class="container"> 
+        <form action="" method="POST" class="login-email">
+            <p style="font-seize:2rem;font-weight:850">LOGIN</p>
+            <div class="input-group"><input type="text" placeholder="User Name" name="username" ></div>
+            <div class="input-group"><input type="password" placeholder="Password" name="password" ></div>
+            <div class="input-group"><button name="submit" class="btn">LOGIN</button></div> 
+            <p class="login-register-text">Don't Have an Account?<a href="register.php">Register</a></p>
+            <p class="ke main"><a href="main.html">ke utama</a></p>
+            <p>Mohon Maaf Tidak Sempurna dan Banyak Kurangnya kak</p>
+    </div>
+</body>
+</html>
---
+++ posttest4/register.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register</title>
+    <link rel="stylesheet" type="" href="registerstyle.css">
+</head>
+<body>
+    <div class="container"> 
+        <form action="" method="POST" class="login-email">
+            <p style="font-seize:2rem;font-weight:850">REGISTER</p>
+            <div class="input-group"><input type="text" placeholder="User Name" name="username" ></div>
+            <div class="input-group"><input type="text" placeholder="Email" name="email" ></div> 
+            <div class="input-group"><input type="date" placeholder="Tanggal Join" name="tanggal"></div>
+            <div class="input-group"><input type="password" placeholder="Password" name="password" ></div>
+            <div class="input-group"><input type="password" placeholder="Confirm Password" name="cpassword" ></div> 
+            <div class="input-group"><button name="submit" class="btn">REGISTER</button></div> 
+            <p class="login-register-text">Have an Account?<a href="index.php">Login</a></p>
+        </form>
+    </div>
+</body>
+</html>
